e042451ce960954d1f22410964bb8dfa7475f254
MaikeSouzaL <MaikeSouzaLeite@gmail.com>
2023-10-26 23:56:19 -0300

    Primeriro_App

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import config from "./config/index.json";
 
 export default function App() {
+  const [nome, setNome] = useState(""); // Estado para o nome
+  const [email, setEmail] = useState(""); // Estado para o email
+  const [dados, setDados] = useState(null);
+  const [erro, setErro] = useState(null);
+
+  useEffect(() => {
+    async function sendServer() {
+      let response = await fetch(config.urlRoot, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          price: 100,
+          name: "Maike de souza leite",
+        }),
+      });
+      let json = await response.json();
+      console.log(json);
+    }
+    sendServer();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>ola</Text>
     </View>
   );
 }
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
